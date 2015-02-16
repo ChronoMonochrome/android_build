df4e57740f748a7b25d2f68bf1263fdd4ce0081e
Sebastian Jena <sebastianjena95@gmail.com>
2015-02-16 13:25:26 +0100

    Debloating the ROM - take two.
    Conflicts:
    	target/product/sdk_base.mk
    
    Change-Id: Ie785b6cfc245cc4ca9550b2130075abb8cc35202

--- target/product/core.mk
+++ target/product/core.mk
@@ -21,8 +21,6 @@
 # core_tiny.mk in addition to core_minimal.mk.
 
 PRODUCT_PACKAGES += \
-    BasicDreams \
-    Browser \
     Calculator \
     Calendar \
     CalendarProvider \
@@ -32,8 +30,6 @@ PRODUCT_PACKAGES += \
     DeskClock \
     DocumentsUI \
     DownloadProviderUi \
-    Email \
-    Exchange2 \
     ExternalStorageProvider \
     FusedLocation \
     InputDevices \
@@ -42,7 +38,6 @@ PRODUCT_PACKAGES += \
     LatinIME \
     Launcher2 \
     ManagedProvisioning \
-    PicoTts \
     PacProcessor \
     libpac \
     PrintSpooler \
