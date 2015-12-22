129438a42c8170d17e8d56f4bbe8d1bf95a2f5b7
Shilin Victor <chrono.monochrome@gmail.com>
2015-12-22 12:06:38 +0300

    Vivid ROM optimizations
    Change-Id: I471bb1c28b71726384691b77a7c015d7e7f97c38

--- envsetup.sh
+++ envsetup.sh
@@ -618,12 +618,7 @@ function lunch()
 
     echo
 
-    if [[ $USE_PREBUILT_CHROMIUM -eq 1 ]]; then
         chromium_prebuilt
-    else
-        # Unset flag in case user opts out later on
-        export PRODUCT_PREBUILT_WEBVIEWCHROMIUM=""
-    fi
 
     fixup_common_out_dir
 
@@ -1566,13 +1561,8 @@ function chromium_prebuilt() {
     export TARGET_DEVICE=$(get_build_var TARGET_DEVICE)
     hash=$T/prebuilts/chromium/$TARGET_DEVICE/hash.txt
 
-    if [ -r $hash ] && [ $(git --git-dir=$T/external/chromium_org/.git --work-tree=$T/external/chromium_org rev-parse --verify HEAD) == $(cat $hash) ]; then
         export PRODUCT_PREBUILT_WEBVIEWCHROMIUM=yes
         echo "** Prebuilt Chromium is up-to-date; Will be used for build **"
-    else
-        export PRODUCT_PREBUILT_WEBVIEWCHROMIUM=no
-        echo "** Prebuilt Chromium out-of-date/not found; Will build from source **"
-    fi
 }
 
 function get_make_command()
@@ -1615,7 +1605,7 @@ function mka() {
             make -j `sysctl hw.ncpu|cut -d" " -f2` "$@"
             ;;
         *)
-            schedtool -B -n 1 -e ionice -n 1 make -k -j `cat /proc/cpuinfo | grep "^processor" | wc -l` "$@"
+            make -j1 -k "$@"
             ;;
     esac
 }
