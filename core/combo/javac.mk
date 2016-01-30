422b0c54091a2e718c239063ce206c74589f6d74
Shilin Victor <chrono.monochrome@gmail.com>
2016-01-30 09:14:14 +0700

    Update java source path
    Change-Id: Ic1e07a27eb78d5ba158c6162fa539d3fef2ee4f7

--- core/combo/javac.mk
+++ core/combo/javac.mk
@@ -9,15 +9,15 @@
 
 # Whatever compiler is on this system.
 ifeq ($(BUILD_OS), windows)
-    COMMON_JAVAC := development/host/windows/prebuilt/javawrap.exe -J-Xmx256m \
+    COMMON_JAVAC := development/host/windows/prebuilt/javawrap.exe -J-Xmx1024m \
         -target 1.5 -source 1.5 -Xmaxerrs 9999999
 else
-    COMMON_JAVAC := javac -J-Xmx512M -target 1.5 -source 1.5 -Xmaxerrs 9999999
+    COMMON_JAVAC := javac -J-Xmx512M -target 1.7 -source 1.7 -Xmaxerrs 9999999
 endif
 
 # Eclipse.
 ifeq ($(CUSTOM_JAVA_COMPILER), eclipse)
-    COMMON_JAVAC := java -Xmx256m -jar prebuilt/common/ecj/ecj.jar -5 \
+    COMMON_JAVAC := java -Xmx1024m -jar prebuilt/common/ecj/ecj.jar -5 \
         -maxProblems 9999999 -nowarn
     $(info CUSTOM_JAVA_COMPILER=eclipse)
 endif
@@ -25,13 +25,13 @@ endif
 # OpenJDK.
 ifeq ($(CUSTOM_JAVA_COMPILER), openjdk)
     # We set the VM options (like -Xmx) in the javac script.
-    COMMON_JAVAC := prebuilt/common/openjdk/bin/javac -target 1.5 \
-        -source 1.5 -Xmaxerrs 9999999
+    COMMON_JAVAC := prebuilt/common/openjdk/bin/javac -target 1.8 \
+        -source 1.8 -Xmaxerrs 9999999
     $(info CUSTOM_JAVA_COMPILER=openjdk)
 endif
    
 HOST_JAVAC ?= $(COMMON_JAVAC)
 TARGET_JAVAC ?= $(COMMON_JAVAC)
-    
+
 #$(info HOST_JAVAC=$(HOST_JAVAC))
 #$(info TARGET_JAVAC=$(TARGET_JAVAC))
