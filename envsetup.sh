0e2d3a8142758540e9d4c81b957ebd48efafd3a2
Shilin Victor <chrono.monochrome@gmail.com>
2016-02-01 23:55:44 +0700

    Change kernel toolchain
    Change-Id: Id3ceb283da8b7a12ddb5590d991bf0b524055093

--- envsetup.sh
+++ envsetup.sh
@@ -168,7 +168,7 @@ function setpaths()
     case $ARCH in
         arm)
             # Legacy toolchain configuration used for ARM kernel compilation
-            toolchaindir=arm/arm-eabi-$targetgccversion/bin
+            toolchaindir=arm/armv7a-linux-gnueabihf-5.2/bin
             if [ -d "$gccprebuiltdir/$toolchaindir" ]; then
                  export ARM_EABI_TOOLCHAIN="$gccprebuiltdir/$toolchaindir"
                  ANDROID_KERNEL_TOOLCHAIN_PATH="$ARM_EABI_TOOLCHAIN":
